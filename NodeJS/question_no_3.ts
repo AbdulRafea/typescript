e49179c02d3b6f553df132607774b1a0989c29d8
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-06 21:15:31 +0500

    Create question_no_3.ts

---
+++ NodeJS/question_no_3.ts
@@ -0,0 +1,3 @@
+let famousperson = "Albert Einstein once said, “A person who never made a mistake never tried anything new.”";
+document.write(famousperson)
+output=Albert Einstein once said, “A person who never made a mistake never tried anything new.”
