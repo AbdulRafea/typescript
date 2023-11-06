0c9f665daadee35525c65e83b765d0ee4def2c12
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-06 21:08:29 +0500

    Create Question-no:2.js

---
+++ NodeJS/Question-no:2.js
@@ -0,0 +1,27 @@
+let person = "Hello Eric, would you like to learn some Python today?";
+document.write(person.toUpperCase()); document.write("<br> <br>");
+document.write(person.toLowerCase());document.write("<br> <br>");
+
+
+function sentenceCase(str) {
+     if ((str === null) || (str === ''))
+         return false;
+     else
+         str = str.toString();
+
+     return str.replace(/\w\S*/g,
+         function (txt) {
+             return txt.charAt(0).toUpperCase() +
+                 txt.substr(1).toLowerCase();
+         });
+ }
+
+// @ts-ignore
+document.write(sentenceCase(person)); document.write("<br> <br>");
+
+Output:    
+      HELLO ERIC, WOULD YOU LIKE TO LEARN SOME PYTHON TODAY?
+
+      hello eric, would you like to learn some python today?
+
+      Hello Eric, Would You Like To Learn Some Python Today?
