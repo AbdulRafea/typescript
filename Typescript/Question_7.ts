5f92ca6a72a4242b22a1459b981d9b48b898ef51
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-08 14:47:57 +0500

    Create Question_7.ts

---
+++ Typescript/Question_7.ts
@@ -0,0 +1,16 @@
+// Add
+let Add = 5 + 3;
+console.log("Add: 5 + 3 = " + Add); 
+// Subtract
+let Subtract = 10 - 2;
+console.log("Subtract: 5 + 3 = " + Subtract);
+// Multiply
+let Multiply = 4 * 2;
+console.log("Multiply: 4 x 2 = " + Multiply); 
+// Divide
+let Divide = 16/2;
+console.log("Divide: 16 / 2" + Divide); 
+output:Add: 5 + 3 = 8
+Subtract: 5 + 3 = 8
+Multiply: 4 x 2 = 8
+Divide: 16 / 2 = 8
