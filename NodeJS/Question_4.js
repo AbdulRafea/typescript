e55e1e7993109f464cc15d87e6e9425a3bfdb240
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-08 13:49:48 +0500

    Create Question_4.js

---
+++ NodeJS/Question_4.js
@@ -0,0 +1,8 @@
+var famous_person = "Albert Einstein";
+
+// Compose the message
+var message = "A person who never made a mistake never tried anything new. - " + famous_person;
+
+// Print the message
+console.log(message);
+output:A person who never made a mistake never tried anything new
