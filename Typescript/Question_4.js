fba32dbc028f6d4d6ddfd4ed7e8429436baf376c
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-08 13:55:50 +0500

    Update Question_4.js

--- Typescript/Question_4.js
+++ Typescript/Question_4.js
@@ -1,8 +1,12 @@
-var famous_person: string[] = "Albert Einstein";
+// Store the famous person's name in a variable
+var famous_person = "Albert Einstein";
 
 // Compose the message
-var message = "A person who never made a mistake never tried anything new. - " + famous_person;
+var message = famous_person + " 'once said, 'Imagination is more important than knowledge.' ";
 
 // Print the message
-console.log(message);
-output:A person who never made a mistake never tried anything new
+
+document.write(message);
+
+Output;
+      Albert Einstein 'once said, 'Imagination is more important than knowledge.'
