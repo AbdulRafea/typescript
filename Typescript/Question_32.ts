2d7bdd70e43382925af6b8faefc6cf37084f671c
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 14:10:31 +0500

    Create Question_32.ts

---
+++ Typescript/Question_32.ts
@@ -0,0 +1,16 @@
+const usernames:string[] = []; // An empty array of usernames
+
+if (usernames.length === 0) {
+  console.log("We need to find some users!");
+} else {
+  for (let i = 0; i < usernames.length; i++) {
+    if (usernames[i] === "admin") {
+      console.log("Hello admin, would you like to see a status report?");
+    } else {
+      console.log(`Hello ${usernames[i]}, thank you for logging in again.`);
+    }
+  }
+}
+
+Output:
+      We need to find some users!
