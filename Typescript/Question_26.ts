5ef9d99b67899ced9e4e04c017cad1e6bdd36362
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 13:04:59 +0500

    Create Question_26.ts

---
+++ Typescript/Question_26.ts
@@ -0,0 +1,23 @@
+                                                              Program 1
+let aleincolour = ("green")
+if (aleincolour === "green"){
+    console.log("player earned 5 points for shooting green alein.");
+
+}else {
+
+    console.log("the player just earn 10 for shooting non colour alein")
+}
+ program1 ountput:
+       The player just earned 5 points."
+_______________________________________________________________________________________________________________________________________________________                   
+                                                                  program2
+    const alien_color = 'red';
+
+if (alien_color === 'green') {
+  console.log('The player just earned 5 points for shooting the green alien.');
+} else {
+  console.log('The player just earned 10 points for shooting the non-green alien.');
+}
+
+Program 2 Output:
+                The player just earned 10 points for shooting the non-green alien.
