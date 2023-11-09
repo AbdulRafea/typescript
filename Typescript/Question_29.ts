547d71a8dddfc624b0d43e3ef10811a0193dd0e1
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 13:25:47 +0500

    Create Question_29.ts

---
+++ Typescript/Question_29.ts
@@ -0,0 +1,30 @@
+// @ts-ignore
+if (favorite_fruits.includes("strawberries")) {
+  console.log("You really like strawberries!");
+}
+
+// @ts-ignore
+if (favorite_fruits.includes("mangoes")) {
+  console.log("You really like mangoes!");
+}
+
+// @ts-ignore
+if (favorite_fruits.includes("apples")) {
+  console.log("You also like apples!");
+} else {
+  console.log("Apples are not in your favorite fruits.");
+}
+
+// @ts-ignore
+if (favorite_fruits.includes("oranges")) {
+  console.log("You really like oranges!");
+} else {
+  console.log("Oranges are not in your favorite fruits.");
+}
+
+Output:
+      You really like bananas
+      You really like strawberries!
+      You really like mangoes!
+      Apples are not in your favorite fruits.
+      Oranges are not in your favorite fruits.
