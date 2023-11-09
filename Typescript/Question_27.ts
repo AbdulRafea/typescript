52b10a6fc65823dcc33d9173a7822ed69eee8841
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 13:06:55 +0500

    Update Question_27.ts

--- Typescript/Question_27.ts
+++ Typescript/Question_27.ts
@@ -1,1 +1,50 @@
+const alien_color = 'green';
 
+// @ts-ignore
+if (alien_color === 'green') {
+  console.log('The player earned 5 points for shooting the green alien.');
+} else { // @ts-ignore
+    if (alien_color === 'yellow') {
+      console.log('The player earned 10 points for shooting the yellow alien.');
+    } else if (alien_color === 'red') {
+      console.log('The player earned 15 points for shooting the red alien.');
+    }
+}
+
+Output:
+      The player earned 5 points for shooting the green alien.
+_____________________________________________________________________________________________________________________________________________________________
+                                                            Version 2 (Yello Alien):
+const alien_color = 'yellow';
+
+// @ts-ignore
+if (alien_color === 'green') {
+  console.log('The player earned 5 points for shooting the green alien.');
+} else { // @ts-ignore
+    if (alien_color === 'yellow') {
+      console.log('The player earned 10 points for shooting the yellow alien.');
+    } else if (alien_color === 'red') {
+      console.log('The player earned 15 points for shooting the red alien.');
+    }
+}
+
+Output:
+      The player earned 10 points for shooting the yellow alien.
+  _____________________________________________________________________________________________________________________________________________________________
+                                                            Version 3 (Red Alien):
+const alien_color = 'red';
+
+// @ts-ignore
+if (alien_color === 'green') {
+  console.log('The player earned 5 points for shooting the green alien.');
+} else { // @ts-ignore
+    if (alien_color === 'yellow') {
+      console.log('The player earned 10 points for shooting the yellow alien.');
+    } else if (alien_color === 'red') {
+      console.log('The player earned 15 points for shooting the red alien.');
+    }
+}
+
+Output:
+      The player earned 15 points for shooting the red alien.
+      
