cc214545fa03b6ed5f78e5f4e9f533cf1a49c4f0
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 09:01:44 +0500

    Create Question_13.ts

---
+++ Typescript/Question_13.ts
@@ -0,0 +1,11 @@
+const favoriteTransportation = ["motorcycle", "car", "bicycle", "scooter", "truck"];
+
+// Iterate through the list and print statements about each item
+favoriteTransportation.forEach(item => {
+    console.log(`I would like to own a ${item}.`);
+});
+output :I would like to own a motorcycle.
+I would like to own a car.
+I would like to own a bicycle.
+I would like to own a scooter.
+would like to oI wn a truck.
