70185d860144bbea5234801f5a4eb878e2027375
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 11:35:50 +0500

    Create Question_20.ts

---
+++ Typescript/Question_20.ts
@@ -0,0 +1,17 @@
+let countries = ["Pakistan","china","canada","America","Palistine","iran","italy","saudia","dubai","afghaistan"];
+console.log("list of countries" )
+for (let i =0; i < countries.length; i++){
+    console.log(`${i + 1}. ${countries[i]}`)
+}
+Output:
+"list of countries" 
+ "1. Pakistan" 
+ "2. china" 
+ "3. canada" 
+ "4. America" 
+ "5. Palistine" 
+ "6. iran" 
+ "7. italy" 
+ "8. saudia" 
+ "9. dubai" 
+ "10. afghaistan"
