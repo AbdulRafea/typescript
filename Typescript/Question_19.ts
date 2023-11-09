63c18cc8264bb68e0a17e6adf0cf6cdef9da464e
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 11:23:04 +0500

    Create Question_19.ts

---
+++ Typescript/Question_19.ts
@@ -0,0 +1,6 @@
+let guests: string[] = ["Alice", "Bob", "Charlie", "David"];
+
+document.write(`You are inviting ${guests.length} guests to dinner.`);
+
+Output:
+      You are inviting 4 guests to dinner.
