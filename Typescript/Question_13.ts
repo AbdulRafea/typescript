f6ba7226bdd8fb009d645dfd6b07729477a04578
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 08:52:35 +0500

    Create Question_13.ts

---
+++ Typescript/Question_13.ts
@@ -0,0 +1,10 @@
+let motercyclebrand = ["honda","suzuki","kawasaki","BMW","yamaha"];
+for (let i=0; i < motercyclebrand.length; i++ ){
+    console.log("i wish to buy"+ motercyclebrand[i] +"and ride it.__");
+
+output:
+"i wish to buyhondaand ride it.__" 
+"i wish to buysuzukiand ride it.__" 
+"i wish to buykawasakiand ride it.__"
+"i wish to buyBMWand ride it.__" 
+"i wish to buyyamahaand ride it.__" 
