141e6e27fd96d9b411e5159921a85e6fb07156d3
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 11:54:01 +0500

    Create Question_23.ts

---
+++ Typescript/Question_23.ts
@@ -0,0 +1,74 @@
+// Test 1: Equality check (True)
+
+alert("See answer in console");
+
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+// Test 2: Inequality check (True)
+let fruit = 'apple';
+console.log("Is fruit != 'banana'? I predict True.");
+console.log(fruit != 'banana');
+
+// Test 3: Greater than check (True)
+let number = 10;
+console.log("Is number > 5? I predict True.");
+console.log(number > 5);
+
+// Test 4: Less than check (True)
+let age = 25;
+console.log("Is age < 30? I predict True.");
+console.log(age < 30);
+
+// Test 5: Logical AND (True)
+let sunny = true;
+let warm = true;
+console.log("Is it sunny AND warm? I predict True.");
+console.log(sunny && warm);
+
+// Test 6: Logical OR (True)
+let raining = false;
+let indoors = true;
+console.log("Is it raining OR are you indoors? I predict True.");
+console.log(raining || indoors);
+
+// Test 7: Equality check (False)
+let color = 'red';
+console.log("Is color == 'blue'? I predict False.");
+console.log(color == 'blue');
+
+// Test 8: Inequality check (False)
+let city = 'New York';
+console.log("Is city != 'New York'? I predict False.");
+console.log(city != 'New York');
+
+// Test 9: Greater than check (False)
+let temperature = 3;
+console.log("Is temperature > 10? I predict False.");
+console.log(temperature > 10);
+
+// Test 10: Less than check (False)
+let price = 50;
+console.log("Is price < 40? I predict False.");
+console.log(price < 40);
+outpu:'subaru'? I predict True." 
+ true 
+ "Is fruit != 'banana'? I predict True." 
+ true 
+ "Is number > 5? I predict True." 
+: true 
+ "Is age < 30? I predict True." 
+ true 
+ "Is it sunny AND warm? I predict True." 
+ true 
+   "Is it raining OR are you indoors? I predict True." 
+ true 
+ "Is color == 'blue'? I predict False." 
+ false 
+ "Is city != 'New York'? I predict False." 
+: false 
+ "Is temperature > 10? I predict False."
+   false 
+ "Is price < 40? I predict False." 
+ false 
