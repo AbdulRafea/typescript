f2a9cbbcab4a2c68898da0904eba95f43ea5ab94
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 11:42:57 +0500

    Create Question_21.ts

---
+++ Typescript/Question_21.ts
@@ -0,0 +1,24 @@
+let person: {name: string, age: number, occupation: string} = {
+  name: "John",
+  age: 30,
+  occupation: "Software Developer"
+};
+console.log(person);
+let car: {make: string, model: string, year: number} = {
+  make: "Honda",
+  model: "Accord",
+  year: 2022
+};
+console.log (car)
+output:
+{
+  "name": "John",
+  "age": 30,
+  "occupation": "Software Developer"
+} 
+{
+
+  "make": "Honda",
+  "model": "Accord",
+  "year": 2022
+} 
