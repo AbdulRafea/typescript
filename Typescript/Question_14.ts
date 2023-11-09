6c68aa86c5e6190821d9b9666fa165546e681683
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-09 09:31:51 +0500

    Create Question_14.ts

---
+++ Typescript/Question_14.ts
@@ -0,0 +1,8 @@
+let DearGest = ["Sir Zai", "Mam Wania", "Sir Muzhar", "Sir Usman", "Other Staff"];
+
+// Loop through the array and print statements 
+  for (let i = 0; i < DearGest.length; i++){
+         let invitation = " DearGest " + DearGest[i] + " I invite you to dinner in my house "
+           console.log(invitation);
+  };
+output:   DearSir ZaiYou invited in the Dinner,DearMam WaniaYou invited in the Dinner,DearSir MuzharYou invited in the Dinner,DearSir UsmanYou invited in the Dinner,DearOther StaffYou invited in the Dinner,
