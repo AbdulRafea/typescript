82733e77d48dafb97cd99363f5c6e0495e9eb980
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-10 20:13:51 +0500

    Create Question_41.ts

---
+++ Typescript/Question_41.ts
@@ -0,0 +1,10 @@
+let magician = ["David Copperfield", "Siegfried and Roy", "Lance Burton" ];
+function show_magicians(magicianArray: any[]){
+    magicianArray.forEach(magician =>
+    console.log(magician));
+}
+
+console.log(magician);
+
+Output:
+      ['David Copperfield', 'Siegfried and Roy', 'Lance Burton']
