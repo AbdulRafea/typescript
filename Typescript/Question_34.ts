3f990f361e710e5707b0091a0acb6490b1230d53
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-10 15:03:47 +0500

    Create Question_34.ts

---
+++ Typescript/Question_34.ts
@@ -0,0 +1,13 @@
+const pizza = ["B.B.Q pizza", "Cheese pizza", "Crown Crust"];
+
+for (let i = 0; i < pizza.length; i++) {
+  document.write(`I like ${pizza[i]} pizza.`);
+}
+
+document.write("I really love pizza!");
+
+Output:
+      I like B.B.Q pizza pizza.
+      I like Cheese pizza pizza.
+      I like Crown Crust pizza.
+      I really love pizza!
