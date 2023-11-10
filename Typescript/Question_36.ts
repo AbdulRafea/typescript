2deb203a84136f8df02543b4dde080276d89cfd1
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-10 15:05:59 +0500

    Create Question_36.ts

---
+++ Typescript/Question_36.ts
@@ -0,0 +1,10 @@
+// We will make a function
+function make_shirt (size: string, message: string){
+     console.log("The size of the shirt is " + size + " and the message printed on it is: '" + message + "'.");
+}
+
+// Call the function with sample arguments
+make_shirt('M', 'Coding is cool!');
+
+Output:
+      The size of the shirt is M and the message printed in it is: Coding is cool!
