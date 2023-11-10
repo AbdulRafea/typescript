2a17cfdbb62685dc225dd4c88eca3cd3682d3833
AbdulRafea <150054177+AbdulRafea@users.noreply.github.com>
2023-11-10 15:08:54 +0500

    Create Question_39.ts

---
+++ Typescript/Question_39.ts
@@ -0,0 +1,15 @@
+// Define the function city_country
+function city_country(city: string, country: string) {
+    return `${city}, ${country}`;
+}
+
+// Call the function with three city-country pairs and print the results
+console.log(city_country('Lahore', 'Pakistan'));
+console.log(city_country('Tokyo', 'Japan'));
+console.log(city_country('Paris', 'France'));
+
+Output:
+      Lahore, Pakistan
+      Tokyo, Japan
+      Paris, France
+  
